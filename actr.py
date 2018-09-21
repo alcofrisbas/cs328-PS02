0c500cde2974b365de8bb3b622ec69f005ff5833
Quinn Schiller <schillerQuinn@gmail.com>
2018-09-21 10:41:35 -0500

    some progress

--- actr.py
+++ actr.py
@@ -8,31 +8,30 @@ Anna Rafferty CS328 21 Sept. 2018
 """
 
 from math import log
-import pandas as pd
 
 
 def baseline_activation(d, lags):
     s = 0
     for i in lags:
-        if str(i) == "inf":
+        if str(i) == "inf": #ignore inf lags
             continue
         else:
                 s += (1.0/(i**d))
     #print(s)
     if s == 0:
-        return -float("Inf")
+        return -float("Inf") #If there is no activation, avoid domain error of log(0) manually
     else:
         return log(s)
-        #return log(sum([1.0/i**d for i in lags]))
+        #return log(sum([1.0/i**d for i in lags])) 
 
 def compute_all_baselines(fname):
     with open(fname) as r:
-        raw = ([i.strip().split(",") for i in r.readlines()])
+        raw = ([i.strip().split(",") for i in r.readlines()]) #read and split the data into a 2d array
         lines = map(list, zip(*raw)) #transpose it so there is a row for each verb
     l = []
     for line in lines:
-        lags = [float(i) for i in  line]
-        l.append(baseline_activation(0.5, lags))
+        lags = [float(i) for i in  line] #turn each number into a float
+        l.append(baseline_activation(0.5, lags)) #make a list of activations and return it
     return l
 
 if __name__ == "__main__":
